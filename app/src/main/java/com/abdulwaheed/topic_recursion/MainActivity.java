556c55de38d842c1b1cb16296a91409842e6a717
babarawan <bawan@abacusoft.com>
2019-07-24 16:22:05 +0500

    Initial Commit

---
+++ app/src/main/java/com/abdulwaheed/topic_recursion/MainActivity.java
@@ -0,0 +1,13 @@
+package com.abdulwaheed.topic_recursion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
